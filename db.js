fff878c9011a971b9f56a033e6d01662dfcfb485
FRAYSSE Ianis <ianis.fraysse@enedis.fr>
2026-08-31 16:00:54 +0200

    Ajout base de donnée et test d'insertion d'une carte

---
+++ db.js
@@ -0,0 +1,17 @@
+const {DatabaseSync} = require('node:sqlite');
+const db = new DatabaseSync('cartes.db');
+db.exec(`
+  CREATE TABLE IF NOT EXISTS cartes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL,
+    jeu TEXT NOT NULL,
+    extention TEXT,
+    rarete TEXT,
+    etat TEXT,
+    quantite INTEGER default 1,
+    valeur_estimee REAL,
+    image_url TEXT
+  );
+`);
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const db = require('./db');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Ma collection de cartes arrive bientôt !');
+});
+// Ajouter une carte de test
+app.get('/test-insert', (req, res) => {
+    const stmt = db.prepare(`INSERT INTO cartes (nom, jeu) VALUES (?, ?)`);
+    stmt.run('Dracaufeu', 'Pokémon');
+    res.send('Carte insérée!');
+});
+
+// Lister toutes les cartes
+app.get('/cartes', (req, res) => {
+    const cartes = db.prepare('SELECT * FROM cartes').all();
+    res.json(cartes);
 });
 
 app.listen(port, () => {
